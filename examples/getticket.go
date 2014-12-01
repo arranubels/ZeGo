9ea4c52953250191e034aa38b72dc45a3f907fb4
arran ubels <a.ubels@base2services.com>
2014-12-02 10:40:47 +1100

    Minor restructure

--- examples/getticket.go
+++ examples/getticket.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/adamar/zego/zego"
+	"github.com/arranubels/zego"
 	"log"
 	"encoding/json"
 )
@@ -10,7 +10,7 @@ func main() {
 	auth := zego.Auth{"user@example.com", "Password", "example.zendesk.com"}
 	// auth := zego.Auth{"user@example.com/token", "Token", "example.zendesk.com"}
 	response, _ := auth.ListTickets()
-	tickets := &zego.TicketArray{}
-	json.Unmarshal([]byte(response.Raw), tickets)
+	tickets := zego.TicketArray{}
+	json.Unmarshal([]byte(response.Raw), &tickets)
 	log.Print(tickets)
 }
