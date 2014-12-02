6e226728e368032576905207ce65a23a1352a1f8
arran ubels <a.ubels@base2services.com>
2014-12-02 13:49:26 +1100

    Added search page

---
+++ search.go
@@ -0,0 +1,30 @@
+package zego
+
+import (
+		"net/url"
+		)
+
+func (a Auth) Search(query string) (*Resource, error) {
+
+	path := "/search.json?query=" + net.QueryEscape(query)
+	resource, err := api(a, "GET", path, "")
+	if err != nil {
+		return nil, err
+	}
+
+	return resource, nil
+
+}
+
+func (a Auth) AnonymousSearch(query string) (*Resource, error) {
+
+	path := "/portal/search.json?query=" + net.QueryEscape(query)
+	resource, err := api(a, "GET", path, "")
+	if err != nil {
+		return nil, err
+	}
+
+	return resource, nil
+
+}
+
