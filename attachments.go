e80a966840b23ecb8ae46702d9b5d8d616638f0c
arran ubels <a.ubels@base2services.com>
2014-12-09 10:17:00 +1100

    Get comments added

---
+++ attachments.go
@@ -0,0 +1,11 @@
+package zego
+
+type Attachment struct {
+        Id                    uint32 
+        File_name             string 
+        Content_url           string 
+        Content_type          string 
+        Size                  string 
+        Thumbnails            string 
+}
+
---
+++ comments.go
@@ -0,0 +1,28 @@
+package zego
+
+type CommentArray struct {
+        Comments       []Comment
+}
+
+type Comment struct {
+        Id                    uint32 
+        Type                  string 
+        Body                  string 
+        Html_body             string 
+        Public                bool 
+        Authur_id             uint32 
+				Attachments           []Attachment
+        Via                   interface{} 
+        Metadata              interface{} 
+        Created_at            string 
+}
+
+func (a Auth) GetTicketComments(ticket_id string) (*Resource, error) {
+	path := "/tickets/" + ticket_id + "/comments.json"
+	resource, err := api(a, "GET", path, "")
+	if err != nil {
+		return nil, err
+	}
+	return resource, nil
+}
+
