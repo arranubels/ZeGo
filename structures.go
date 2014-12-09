d40759c6d6474280e2477895a79d9fdc7bf53e71
arran ubels <a.ubels@base2services.com>
2014-12-09 16:46:25 +1100

    Now can add filters to a user query

--- structures.go
+++ structures.go
@@ -7,3 +7,25 @@ type Pagination struct {
         Previous_page string 
 }
 
+type Via struct {
+	Channel string
+	Source Source
+}
+
+type Source struct {
+	To SourceObject
+	From SourceObject
+	Rel string
+}
+
+type SourceObject struct {
+	Address string
+	Name string
+	Id string
+	Title string
+	Topic_id string
+	Topic_name string
+	Original_recipients string
+	Profile_url string
+	Username string
+}
--- users.go
+++ users.go
@@ -54,6 +54,17 @@ func (a Auth) ListUsers() (*Resource, error) {
 
 }
 
+func (a Auth) FilterUsers(filters string) (*Resource, error) {
+	path := "/users.json?" + filters
+	resource, err := api(a, "GET", path, "")
+	if err != nil {
+		return nil, err
+	}
+
+	return resource, nil
+
+}
+
 func (a Auth) ShowUser(user_id string) (*Resource, error) {
 
 	path := "/users/" + user_id + ".json"
